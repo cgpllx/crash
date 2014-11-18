3ce0993364cec2bddda027eeab3d8eaac3825835
cgpllx <cgpllx1@qq.com>
2014-11-18 17:39:33 +0800

    Signed-off-by: cgpllx <cgpllx1@qq.com>

--- src/com/kubeiwu/crash/MainActivity.java
+++ src/com/kubeiwu/crash/MainActivity.java
@@ -1,22 +1,15 @@
 package com.kubeiwu.crash;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
 
 public class MainActivity extends Activity {
-	private String s;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		// System.out.println(s.equals("any string"));
 		setContentView(R.layout.activity_main);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
+ 
 }
